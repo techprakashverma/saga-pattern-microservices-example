629f888014388e8ab52c4c3fae89d9ecc97c8419
Prakash <tech.prakashverma@gmail.com>
2026-03-16 00:54:49 +0530

    add CircuitBreaker in api-gateway

---
+++ api-gateway/src/main/java/com/gateway/ms/controller/FallbackController.java
@@ -0,0 +1,16 @@
+package com.gateway.ms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @GetMapping("/order")
+    public String orderFallback() {
+        return "Order Service is temporarily unavailable. Please try again later.";
+    }
+
+}
